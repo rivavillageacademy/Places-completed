c530eab75b5b5da89dd81f55206ad25fff91cd5f
Muhamed Alkhatib <malkhatib@gmail.com>
2020-09-11 15:07:15 +0400

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Muhamed Alkhatib on 11/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
